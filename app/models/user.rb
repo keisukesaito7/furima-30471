afe73dccf890692acdbf70a3f70420dbabdd198a
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-02 08:29:54 +0900

    バリデーション修正＆Userモデル単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  eng_num = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+{6}\z/i
+  eng_num = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
   zenkaku = /\A[ぁ-んァ-ン一-龥]+\z/
   zenkaku_kana = /\A[ァ-ン]+\z/
   birthday = /\A\d{4}-\d{2}-\d{2}\z/
   validates :nickname,        presence: true
-  validates :password,        presence: true, format: {with: eng_num}
+  validates :password,                        format: {with: eng_num}
   validates :first_name,      presence: true, format: {with: zenkaku}
   validates :last_name,       presence: true, format: {with: zenkaku}
   validates :first_name_kana, presence: true, format: {with: zenkaku_kana}
