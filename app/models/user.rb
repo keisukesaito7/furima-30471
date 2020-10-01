ae1769074ee5da780376f102215784d636276a8b
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-01 21:57:39 +0900

    rspecのインストール

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,12 @@ class User < ApplicationRecord
   zenkaku = /\A[ぁ-んァ-ン一-龥]+\z/
   zenkaku_kana = /\A[ァ-ン]+\z/
   birthday = /\A\d{4}-\d{2}-\d{2}\z/
-  validates :nickname, presence: true
-  validates :password, presence: true, format: {with: eng_num}
-  validates :first_name, presence: true, format: {with: zenkaku}
-  validates :last_name, presence: true, format: {with: zenkaku}
+  validates :nickname,        presence: true
+  validates :password,        presence: true, format: {with: eng_num}
+  validates :first_name,      presence: true, format: {with: zenkaku}
+  validates :last_name,       presence: true, format: {with: zenkaku}
   validates :first_name_kana, presence: true, format: {with: zenkaku_kana}
-  validates :last_name_kana, presence: true, format: {with: zenkaku_kana}
-  validates :birth_date, presence: true, format: {with: birthday}
+  validates :last_name_kana,  presence: true, format: {with: zenkaku_kana}
+  validates :birth_date,      presence: true, format: {with: birthday}
 
 end
