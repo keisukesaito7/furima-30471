9a3887e4964084b4376f613ea97aba13fdb0a7bb
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-02 19:39:10 +0900

    販売手数料/販売利益をリアルタイムに更新するJSファイル作成

---
+++ app/javascript/calculate.js
@@ -0,0 +1,15 @@
+function calculate() {
+  const price = document.getElementById("item-price");
+  const taxPrice = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+
+  price.addEventListener("keyup", () => {
+    const tax = 0.10;
+    const changedTaxPrice = parseInt(price.value * tax)
+    taxPrice.innerHTML = changedTaxPrice;
+    const changedProfit = price.value - changedTaxPrice;
+    profit.innerHTML = changedProfit;
+  })
+}
+
+window.addEventListener("load", calculate)
