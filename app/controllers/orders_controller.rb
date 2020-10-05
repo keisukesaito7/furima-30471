4a8b94bcaaf9314a66ee84cb605b2fc0e316a555
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-05 15:46:41 +0900

    ordersコントローラの#createアクションとストロングパラメータを定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,21 @@
 class OrdersController < ApplicationController
   def new
     @item = Item.find(params[:item_id])
+    @order = ItemOrder.new
   end
 
   def create
+    @order = ItemOrder.new(order_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def order_params
+    params.require(:item_order).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
