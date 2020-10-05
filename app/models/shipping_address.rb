e108e561ecc88538db2fe917a091a62691988b3b
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-05 14:04:55 +0900

    ShippingAddressモデルのバリデーションを定義

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -2,4 +2,14 @@ class ShippingAddress < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :order
   belongs_to_active_hash :prefecture
+
+  with_options presence: true do
+    validates :postal_code,  format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefecture
+    validates :city
+    validates :addresses
+    validates :phone_number, format: { with: /\A\d{,11}\z/ }
+  end
+
+  validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
 end
