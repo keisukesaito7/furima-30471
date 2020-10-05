1ad5cbd3e25b6e46498e7f72c1d3feb03af12d0f
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-05 14:30:51 +0900

    ItemOrderモデルにorderとshipping_addressを保存する機能を実装

--- app/forms/item_order.rb
+++ app/forms/item_order.rb
@@ -10,4 +10,9 @@ class ItemOrder
     validates :phone_number, format: { with: /\A\d{,11}\z/ }
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
+
+  def save
+    order = Order.create(user_id: current_user.id, item_id: params[:item_id])
+    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
+  end
 end
