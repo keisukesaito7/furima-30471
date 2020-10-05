24b97edc14f418742cec0f6eaf435416f68fd8dd
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-05 16:04:56 +0900

    ①ログアウト②出品者に対するリダイレクトを実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :new
+  before_action :redirect_to_root_path, only: :new
+
   def new
     @item = Item.find(params[:item_id])
     @order = ItemOrder.new
@@ -17,5 +20,10 @@ class OrdersController < ApplicationController
   private
   def order_params
     params.require(:item_order).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def redirect_to_root_path
+    item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == item.user.id
   end
 end
