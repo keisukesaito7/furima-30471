eddd61b2e620e6e36fab0112bbbe1c2840a0abc6
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-05 14:00:59 +0900

    with_optionsでUserモデルのバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,15 @@ class User < ApplicationRecord
   zenkaku = /\A[ぁ-んァ-ン一-龥]+\z/
   zenkaku_kana = /\A[ァ-ン]+\z/
   birthday = /\A\d{4}-\d{2}-\d{2}\z/
-  validates :nickname, presence: true
-  validates :password,                        format: { with: eng_num }
-  validates :first_name,      presence: true, format: { with: zenkaku }
-  validates :last_name,       presence: true, format: { with: zenkaku }
-  validates :first_name_kana, presence: true, format: { with: zenkaku_kana }
-  validates :last_name_kana,  presence: true, format: { with: zenkaku_kana }
-  validates :birth_date,      presence: true, format: { with: birthday }
+  with_options presence: true do
+    validates :nickname
+    validates :first_name,      format: { with: zenkaku }
+    validates :last_name,       format: { with: zenkaku }
+    validates :first_name_kana, format: { with: zenkaku_kana }
+    validates :last_name_kana,  format: { with: zenkaku_kana }
+    validates :birth_date,      format: { with: birthday }
+  end
+  validates :password,          format: { with: eng_num }
 
   has_many :items
   has_many :orders
