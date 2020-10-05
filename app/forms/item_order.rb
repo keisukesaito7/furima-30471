4ed8f32c69e87faec7c8ab32f69034a741dde600
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-05 14:20:43 +0900

    ItemOrderモデルにバリデーションを設定

--- app/forms/item_order.rb
+++ app/forms/item_order.rb
@@ -1,4 +1,13 @@
 class ItemOrder
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number
+
+  with_options presence: true do
+    validates :postal_code,  format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefecture
+    validates :city
+    validates :addresses
+    validates :phone_number, format: { with: /\A\d{,11}\z/ }
+  end
+  validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -3,13 +3,4 @@ class ShippingAddress < ApplicationRecord
   belongs_to :order
   belongs_to_active_hash :prefecture
 
-  with_options presence: true do
-    validates :postal_code,  format: { with: /\A\d{3}[-]\d{4}\z/ }
-    validates :prefecture
-    validates :city
-    validates :addresses
-    validates :phone_number, format: { with: /\A\d{,11}\z/ }
-  end
-
-  validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
 end
