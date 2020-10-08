cf3d565d6ca1f342396114cb25b923f65fb30dee
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-08 17:53:50 +0900

    ログインしている本人のみが詳細ページへアクセス可能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+  before_action :set_user, only: [:show]
+  before_action :redirect_to_root_path, only: [:show]
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def redirect_to_root_path
+    if current_user.id != @user.id
+      redirect_to root_path
+    end
   end
 
 end
