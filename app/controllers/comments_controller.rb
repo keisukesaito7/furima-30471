38117e9e3a17bca3729a15ee2063b08ca73188be
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-09 19:04:05 +0900

    commentsコントローラにcreateアクションを定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
-class CommentsController < ApplicationController  
+class CommentsController < ApplicationController
+  
+  def create
+    @message = Message.new(message_params)
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
