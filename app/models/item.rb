66a0e977626315bae99f02dcd0bf1221a65757e6
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-09 19:00:49 +0900

    User, Item, Commentモデルのアソシエーションを定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :scheduled_delivery
   has_one                :order
+  has_many               :comments, dependent: :destroy
 
   with_options presence: true do
     validates :image
