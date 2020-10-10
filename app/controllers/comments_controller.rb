9fe0345624b9739d2642eb1b923cfca9d90f774d
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-10 23:02:49 +0900

    ストロングパラメータを修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,7 +12,7 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
